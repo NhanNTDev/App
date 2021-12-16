49e1824a78667678c7ab5e0efb03dccdc5292a9d
TonyApu <you@example.com>
2021-12-17 00:20:47 +0700

    add category-service, bug

--- src/components/TopCategory.js
+++ src/components/TopCategory.js
@@ -1,74 +1,36 @@
-const TopCategory = () => {
-  let categories = [
-    {
-      id: "1",
-      title: "Rau Ăn Lá",
-      totalItem: "100",
-      image: "img/categories/rau_an_la.PNG",
-    },
-    {
-      id: "2",
-      title: "Rau Hoa Quả",
-      totalItem: "100",
-      image: "img/categories/rau_hoa_qua.PNG",
-    },
-    {
-      id: "3",
-      title: "Rau Củ Gia Vị",
-      totalItem: "100",
-      image: "img/categories/rau_gia_vi.PNG",
-    },
-    {
-      id: "4",
-      title: "Củ",
-      totalItem: "100",
-      image: "img/categories/cu.PNG",
-    },
-    {
-      id: "5",
-      title: "Trái Cây",
-      totalItem: "100",
-      image: "img/categories/trai_cay.PNG",
-    },
-    {
-      id: "6",
-      title: "Hạt",
-      totalItem: "100",
-      image: "img/categories/hat.PNG",
-    },
-    {
-      id: "7",
-      title: "Nấm",
-      totalItem: "100",
-      image: "img/categories/nam.PNG",
-    },
-    {
-      id: "8",
-      title: "Hoa Tươi",
-      totalItem: "100",
-      image: "img/categories/hoa_tuoi.PNG",
-    },
-    {
-      id: "9",
-      title: "Khác",
-      totalItem: "100",
-      image: "img/categories/khac.PNG",
-    },
-  ];
+import React, { useEffect, useState } from "react";
+import * as categoryService from "../services/category-service";
+import { runScript } from "../utils/Common";
 
-  function renderItem (props) {
+const TopCategory = () => {
+  
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetChCategories = async () => {
+      const categoryResponse = await categoryService.getAllCategoriesAPI();
+      setCategories(categoryResponse);
+    };
+    fetChCategories();
+  }, [])
+
+  function renderItem(props) {
+    console.log('category');
+    console.log(props);
     return (
       <div className="item" key={props.id}>
         <div className="category-item">
           <a href="shop.html">
             <img className="img-fluid" src={props.image} alt="" />
-            <h6>{props.title}</h6>
+            <h6>{props.name}</h6>
             <p>{props.totalItem} Sản Phẩm</p>
           </a>
         </div>
       </div>
     );
-  };
+  }
+
+
   return (
     <>
       <section className="top-category section-padding">
