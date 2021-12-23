a6688f9bcf0610ccf1088804ecb801ddf7f5bc9d
NhanNTDev <nguyenthanhnhan6761@gmail.com>
2021-12-23 11:28:44 +0700

    update common.js

--- src/utils/Common.js
+++ src/utils/Common.js
@@ -2,10 +2,10 @@ export const runScript = () => {
   var body = document.getElementsByTagName("body")[0];
   var script = document.createElement("script");
   script.src = "/vendor/owl-carousel/owl.carousel.js";
-  script.id = "/vendor_carousel";
+  script.id = "vendor_carousel";
   script.type = "text/javascript";
   var script1 = document.createElement("script");
-  script1.src = "js/custom.js";
+  script1.src = "/js/custom.js";
   script1.id = "custom";
   script1.type = "text/javascript";
   console.log("RunScrip");
