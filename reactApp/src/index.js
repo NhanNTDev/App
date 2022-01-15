e4bee8519b8a65539622e235e6cc2dafd86dad2f
NhanNTDev <nguyenthanhnhan6761@gmail.com>
2022-01-15 22:05:19 +0700

    Implement private route and authcontext

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
+// import { store } from "./app/store";
+// import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App/>
-      </Provider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
